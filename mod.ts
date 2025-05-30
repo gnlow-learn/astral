eed03c29ae01fa30e1df9c7c3b084e87e8b30dd7
Gnlow <gnlow@pm.me>
2025-05-30 17:13:03 +0900

    set dimension

--- mod.ts
+++ mod.ts
@@ -4,5 +4,12 @@ await using browser = await launch()
 
 await using page = await browser.newPage("https://deno.land")
 
+const dimension = {
+    width: 640,
+    height: 360,
+}
+
+page.setViewportSize(dimension)
+
 const screenshot = await page.screenshot()
 await Deno.writeFile("temp/a.png", screenshot)
