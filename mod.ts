8f2cbab172295fea0540da369729d265a01d3ddd
Gnlow <gnlow@pm.me>
2025-07-25 01:01:51 +0900

    pixelmatch

--- mod.ts
+++ mod.ts
@@ -1,4 +1,6 @@
 import { launch } from "jsr:@astral/astral@0.5.3"
+import pixelmatch from "https://esm.sh/pixelmatch@7.1.0"
+import sharp from "npm:sharp@0.34.3"
 
 await using browser = await launch()
 
@@ -12,4 +14,18 @@ const dimension = {
 page.setViewportSize(dimension)
 
 const screenshot = await page.screenshot()
-await Deno.writeFile("temp/a.png", screenshot)
+// await Deno.writeFile("temp/a.png", screenshot)
+
+const process =
+(buf: Uint8Array<ArrayBufferLike>) =>
+    sharp(buf).ensureAlpha().png().raw().toBuffer()
+
+console.log(
+    pixelmatch(
+        await process(screenshot),
+        await process(await Deno.readFile("temp/a.png")),
+        void 0,
+        640,
+        360,
+    )
+)
