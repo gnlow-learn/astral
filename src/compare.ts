950f86cdcf78e872bc181c364c2b161b99b1e7da
Gnlow <gnlow@pm.me>
2025-07-25 02:39:04 +0900

    diff

--- src/compare.ts
+++ src/compare.ts
@@ -1,9 +1,10 @@
 import pixelmatch from "https://esm.sh/pixelmatch@7.1.0"
 import sharp from "npm:sharp@0.34.3"
+import { PNG } from "https://esm.sh/pngjs@7.0.0"
 
 const process =
-(buf: Uint8Array<ArrayBufferLike>) =>
-    sharp(buf).ensureAlpha().png().raw().toBuffer()
+(img: sharp.Sharp) =>
+    img.ensureAlpha().png().raw().toBuffer()
 
 export const compare =
 async (
@@ -11,13 +12,26 @@ async (
     b0: Uint8Array<ArrayBufferLike>,
     option?: Parameters<typeof pixelmatch>[5],
 ) => {
+    const a = await process(sharp(a0))
     const { width, height } = await sharp(a0).metadata()
-    return pixelmatch(
-        await process(a0),
-        await process(b0),
-        void 0,
+
+    const b = await process(sharp(b0)
+        .resize(width, height, { fit: "fill" })
+    )
+
+    const diff = new PNG({ width, height })
+    
+    const count = pixelmatch(
+        a,
+        b,
+        diff.data,
         width,
         height,
         option,
     )
+
+    return {
+        diff: PNG.sync.write(diff),
+        count,
+    }
 }
